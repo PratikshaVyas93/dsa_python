45a331215d0a3e57139f0d8f4e9c9cdc4269edfa
Pratiksha <pratiksha@Pratikshas-MacBook-Air.local>
2026-04-03 12:09:36 +0100

    Add variabl.py and variable.md

---
+++ variabl.py
@@ -0,0 +1,21 @@
+# sum of two number
+
+a = int(input("enter value 1: "))
+b = int(input("enter value 2: "))
+print("result: ",a+b)
+
+# even or odd can be devided by 2 or not
+
+n= int(input("enter value : "))
+if (n % 2 == 0):
+    print(f"Number {n} is even")
+else:
+    print(f"Number {n} is odd")
+
+
+x = int(input("enter x value: "))
+y=  int(input("enter y value: "))
+if (x > y):
+    print("x is greater than y")
+else:
+    print("y is greater than x")
---
+++ variable.md
@@ -0,0 +1,7 @@
+variable: is used to store data
+
+☐ Learned variables  
+☐ Took user input  
+☐ Solved sum problem  
+☐ Solved even/odd  
+☐ Solved max problem  
